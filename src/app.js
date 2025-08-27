81425e546e39be091a8bb72195d360e98a5aa7d3
zubair arif <104222183+ZubairArif234@users.noreply.github.com>
2025-08-27 20:48:18 +0500

    fix

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use(cors({
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     
-    if (allowedOrigins.indexOf(origin) !== -1 || process.env.NODE_ENV === 'development') {
+    if ( allowedOrigins.some(o => origin?.startsWith(o)) || process.env.NODE_ENV === 'development') {
       callback(null, true);
     } else {
       console.log('CORS blocked origin:', origin);
