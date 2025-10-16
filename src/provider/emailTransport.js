7955b140cb0ccf95b0ac3871d39c315837f87bbf
zubair arif <104222183+ZubairArif234@users.noreply.github.com>
2025-10-16 22:32:47 +0500

    mmm

--- src/provider/emailTransport.js
+++ src/provider/emailTransport.js
@@ -1,27 +1,27 @@
 
-// import nodemailer from "nodemailer";
+// // import nodemailer from "nodemailer";
 
-const nodemailer = require("nodemailer")
-const dotenv = require("dotenv");
-dotenv.config({ path: "./src/config/config.env" });
+// const nodemailer = require("nodemailer")
+// const dotenv = require("dotenv");
+// dotenv.config({ path: "./src/config/config.env" });
  
 
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",                  
-  auth: {
-    user: process.env.EMAIL,          
-    pass: process.env.APP_PASSWORD,  
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",                  
+//   auth: {
+//     user: process.env.EMAIL,          
+//     pass: process.env.APP_PASSWORD,  
+//   },
+// });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("Error with mail transporter config:", error);
-  } else {
-    console.log("Mail transporter is ready to send emails");
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.error("Error with mail transporter config:", error);
+//   } else {
+//     console.log("Mail transporter is ready to send emails");
+//   }
+// });
 
 
-module.exports = transporter;
+// module.exports = transporter;
